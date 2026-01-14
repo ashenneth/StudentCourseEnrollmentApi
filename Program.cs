b43ffc39300f0e4e3057827230e91c0961b80df3
ashenvn <vimukthiashen35@gmail.com>
2026-01-14 23:44:39 +0530

    update DI Services.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
 using StudentCourseEnrollmentApi.Common;
+using StudentCourseEnrollmentApi.Data;
+using StudentCourseEnrollmentApi.Repositories.Interfaces;
+using StudentCourseEnrollmentApi.Repositories;
+using StudentCourseEnrollmentApi.Services.Interfaces;
+using StudentCourseEnrollmentApi.Services;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,9 +22,23 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    var cs = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseSqlite(cs);
+});
+
+// DI - Repositories
+builder.Services.AddScoped<IStudentRepository, StudentRepository>();
+builder.Services.AddScoped<ICourseRepository, CourseRepository>();
+builder.Services.AddScoped<IEnrollmentRepository, EnrollmentRepository>();
+
+// DI - Services
+builder.Services.AddScoped<IStudentService, StudentService>();
+builder.Services.AddScoped<ICourseService, CourseService>();
+builder.Services.AddScoped<IEnrollmentService, EnrollmentService>();
 
 var app = builder.Build();
-
 
 app.UseSwagger();
 app.UseSwaggerUI();
